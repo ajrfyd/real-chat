fb55d6e9f8a264aff2a1e907e6ed373f934b5e96
ajrfyd <ajrfyd@naver.com>
2022-09-06 23:36:27 +0900

    update: updagte

--- public/src/App.js
+++ public/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/register' element={<Register />}/>
         <Route path='/login' element={<Login />}/>
-        <Route path='/setAvatar' element={<SetAvatar />}/>
+        <Route path='/setavatar' element={<SetAvatar />}/>
         <Route path='/' element={<Chat />}/>
       </Routes>
     </div>
